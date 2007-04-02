2e1ed1da2c93d0d68e1eed9577fd62f99c39a240
zerofrog <zerofrog@23c756db-88ba-2448-99d7-e6e4c676ec84>
2007-04-02 01:06:17 +0000

    pcsx2 0.9.3 and a lot of other additions

---
+++ plugins/pad/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo -----------------------
+echo Building PAD plugins...
+echo -----------------------
+
+curdir=`pwd`
+
+cd ${curdir}/PADwin
+sh build.sh $@
+
+if [ $? -ne 0 ]
+then
+exit 1
+fi
+
+cd ${curdir}/zeropad
+sh build.sh $@
+
+if [ $? -ne 0 ]
+then
+exit 1
+fi
